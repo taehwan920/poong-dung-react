af034b3a3a4cc2882c60e369b47f260a9f373d7d
Yang Taehwan <taehwan920@gmail.com>
2020-04-13 20:37:49 +0900

    Fix bug on get_data.py

--- nodejs/pythons/get_data.py
+++ nodejs/pythons/get_data.py
@@ -1,11 +1,4 @@
 import requests
-import json
-
-start_page = 0
-end_page = 999
-endpoint = f"http://openapi.seoul.go.kr:8088/566261794c7461653638534c656f6d/JSON/WPOSInformationTime/{start_page}/{end_page}"
-
-arr_data = []
 
 
 def is_joong(obj):
@@ -22,7 +15,7 @@ def refine_obj(obj):
     return {
         'date': obj['MSR_DATE'],
         'time': int(obj['MSR_TIME'][0:2]),
-        'temperature': float(obj['W_TEMP'])
+        'temperature': float(obj['W_TEMP']) if obj['W_TEMP'] != '점검중' else 99.9
     }
 
 
@@ -33,10 +26,19 @@ def obj_factory(li):
     return refine3
 
 
+start_page = 0
+end_page = 999
+
+
+arr_data = []
+
 while end_page < 4000:
+    endpoint = f"http://openapi.seoul.go.kr:8088/566261794c7461653638534c656f6d/JSON/WPOSInformationTime/{start_page}/{end_page}"
     response = requests.get(endpoint)
     data = response.json()["WPOSInformationTime"]["row"]
     refined = obj_factory(data)
     arr_data += refined
     start_page += 1000
     end_page += 1000
+
+arr_data.reverse()
