93f1da1404064c34040ae0c37db6638cf1bd55f7
Yang Taehwan <taehwan920@gmail.com>
2020-04-16 15:55:39 +0900

    Refactor AxisX func in canvas.js and modify drawGraph func

--- src/pages/StatisticsComponents/Canvas.js
+++ src/pages/StatisticsComponents/Canvas.js
@@ -5,15 +5,14 @@ const Standard = {
     canvasHeight: 500,
     frameStartX: 100,
     frameEndX: 900,
-    frameOnY: 425,
-    lineStartY: 125,
-    lineEndY: 365, /* this.frameOnY - this.lineInterval */
-    lineInterval: 60, /* (this.frameOnY - this.lineStartY) / 5 */
+    frameStartY: 125,
+    frameEndY: 425,
+    frameIntervalY: 60,
     frameLineWidth: 2,
     frameStrokeStyle: 'rgba(255, 255, 255, 0.5)',
     textFillStyle: 'rgba(255, 255, 255, 1)',
     dailyColor: '#C5F895',
-    weekColor: '#9972AE',
+    monthlyColor: '#9972AE',
     colorLineWidth: 3,
     tickEndY: 430,
     tickStartY: 420,
@@ -21,6 +20,37 @@ const Standard = {
     tickIntervalX: 87.5, /* (this.frameEndX - 200) / 8 */
     tickEndX: 812.5, /* this.frameEndX - this.tickInterval */
 };
+
+export function AxisX(ctx) {
+    const gphAxis = {
+        startX: Standard.frameStartX,
+        endX: Standard.frameEndX,
+        startY: Standard.frameStartY,
+        endY: Standard.frameEndY,
+        intervalY: Standard.frameIntervalY
+    }
+
+    const temps = ['0℃', '5℃', '10℃', '15℃', '20℃', '25℃'];
+
+    for (let i = gphAxis.endY, j = 0; i >= gphAxis.startY; i -= gphAxis.intervalY, j++) {
+        ctx.beginPath();
+        ctx.moveTo(gphAxis.startX, i);
+        ctx.lineTo(gphAxis.endX, i);
+        ctx.lineWidth = Standard.frameLineWidth;
+        ctx.strokeStyle = Standard.frameStrokeStyle;
+        ctx.stroke();
+        ctx.closePath();
+
+        //temperature parameter
+        ctx.font = '15.5px sans-serif';
+        ctx.textAlign = 'end';
+        ctx.textBaseline = 'middle'
+        ctx.fillStyle = Standard.textFillStyle
+        ctx.fillText(temps[j], gphAxis.startX - 10, i);
+    };
+};
+
+
 
 
 export function Title(ctx) {
@@ -33,7 +63,7 @@ export function Title(ctx) {
 export function LineColor(ctx) {
     const eg = {
         dailyColor: Standard.dailyColor,
-        weekColor: Standard.weekColor,
+        monthlyColor: Standard.monthlyColor,
         textX: 940,
         StartX: 800,
         EndX: 840,
@@ -47,7 +77,7 @@ export function LineColor(ctx) {
     ctx.textBaseline = 'middle'
     ctx.fillStyle = Standard.textFillStyle;
     ctx.fillText('일일 변동 수온', eg.textX, eg.positionY1);
-    ctx.fillText('주간 평균 수온', eg.textX, eg.positionY2);
+    ctx.fillText('월간 평균 수온', eg.textX, eg.positionY2);
 
 
     ctx.beginPath();
@@ -62,57 +92,10 @@ export function LineColor(ctx) {
     ctx.moveTo(eg.StartX, eg.positionY2);
     ctx.lineTo(eg.EndX, eg.positionY2);
     ctx.lineWidth = eg.lineWidth;
-    ctx.strokeStyle = eg.weekColor;
+    ctx.strokeStyle = eg.monthlyColor;
     ctx.stroke();
     ctx.closePath();
 };
-
-
-
-export function AxisX(ctx) {
-    const axisX = {
-        startX: Standard.frameStartX,
-        endX: Standard.frameEndX,
-        onY: Standard.frameOnY
-    }
-    //axis X
-    ctx.beginPath();
-    ctx.moveTo(axisX.startX, axisX.onY);
-    ctx.lineTo(axisX.endX, axisX.onY);
-    ctx.lineWidth = Standard.frameLineWidth;
-    ctx.strokeStyle = Standard.frameStrokeStyle;
-    ctx.stroke();
-
-    //axis X - graph lines
-    const graphAxis = {
-        startY: Standard.lineStartY,
-        endY: Standard.lineEndY,
-        intervalY: Standard.lineInterval
-
-    }
-
-    const temps = ['-10℃', '0℃', '10℃', '20℃', '30℃'];
-
-    for (let i = graphAxis.endY, j = 0; i >= graphAxis.startY; i -= graphAxis.intervalY, j++) {
-        ctx.beginPath();
-        ctx.moveTo(axisX.startX, i);
-        ctx.lineTo(axisX.endX, i);
-        ctx.lineWidth = Standard.frameLineWidth;
-        ctx.strokeStyle = Standard.frameStrokeStyle;
-        ctx.stroke();
-        ctx.closePath();
-
-        //temperature parameter
-        ctx.font = '15.5px sans-serif';
-        ctx.textAlign = 'end';
-        ctx.textBaseline = 'middle'
-        ctx.fillStyle = Standard.textFillStyle
-        ctx.fillText(temps[j], axisX.startX - 10, i);
-    };
-};
-
-
-
 
 export function TickAndParams(ctx) {
     //hours ticks
@@ -155,29 +138,33 @@ export const DrawGraph = async (ctx) => {
     const gph = {
         endX: Standard.tickEndX,
         intervalX: Standard.tickIntervalX,
-        stdY: Standard.lineEndY - Standard.lineStartY,
-        const: 2880
+        intervalY: Standard.frameIntervalY,
+        startY: Standard.frameStartY,
+        stdY: Standard.frameEndY - Standard.frameStartY
     };
 
     const endpoint = `http://localhost:8080/db/1/30`;
     const datas = await Axios.get(endpoint);
 
-    const latestDatas = datas.data.slice(0, 8);
-    latestDatas.forEach(item => item.date = `${item.date.slice(-4, -2)}.${item.date.slice(-2)}`);
-    latestDatas.forEach(item => item.time = `${item.time < 10 ? `0${item.time}시` : `${item.time}시`}`);
-    const latestTemp = latestDatas.map(item => item.temperature);
+    const dailyDatas = datas.data.slice(0, 8).reverse(); //시간 순 정렬
+    dailyDatas.forEach(item => item.date = `${item.date.slice(-4, -2)}.${item.date.slice(-2)}`);
+    dailyDatas.forEach(item => item.time = `${item.time < 10 ? `0${item.time}시` : `${item.time}시`}`);
+    const dailyTemps = dailyDatas.map(item => item.temperature);
 
-    for (let i = 7; i >= 0; i--) {
-        const temperature = tempCheck(latestTemp[i], i);
+    const XYs = [];
+
+    for (let i = 0; i < dailyTemps.length; i++) {
+        const dailyTemp = tempCheck(dailyTemps[i], i);
         let dailyX = gph.endX - (gph.intervalX * i)
-        let dailyY = (temperature / gph.stdY * gph.const);
+        let dailyY = (gph.intervalY * (25 - dailyTemp) / 5) + gph.startY;
         if (i === 0) {
             dailyX = gph.endX
         }
-        drawPoint(ctx, dailyX, dailyY);
+        drawDailyPoint(ctx, dailyX, dailyY);
+        XYs.push({ X: dailyX, Y: dailyY });
+    };
+    drawDailyLine(ctx, XYs);
 
-
-    }
 }
 
 
@@ -191,15 +178,26 @@ function tempCheck(temp, num) {
     }
 }
 
-function drawPoint(ctx, X, Y) {
+function drawDailyPoint(ctx, X, Y) {
     ctx.beginPath();
-    ctx.arc(X, Y, 5, Y - 5, Math.PI, true);
+    ctx.arc(X, Y, 6, Y - 5, Math.PI, true);
     ctx.lineWidth = Standard.colorLineWidth;
     ctx.fillStyle = Standard.dailyColor;
     ctx.fill();
     ctx.closePath();
 }
 
-// function drawLine(ctx, X, Y) {
-//     const rightEnd = []
-// }
+function drawDailyLine(ctx, arr) {
+    let leftEnd = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        ctx.beginPath();
+        ctx.moveTo(leftEnd.X, leftEnd.Y);
+        leftEnd = arr[i]
+        ctx.lineTo(leftEnd.X, leftEnd.Y);
+        ctx.lineWidth = 4;
+        ctx.strokeStyle = Standard.dailyColor;
+        ctx.stroke();
+        ctx.closePath();
+    }
+};
