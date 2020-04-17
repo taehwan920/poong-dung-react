db8012c5c9127c5b44c28075f87b75834e2b23c3
Yang Taehwan <taehwan920@gmail.com>
2020-04-17 18:48:52 +0900

    Add homebtn on error page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -9,6 +9,11 @@ export default class extends React.Component {
             <center className="home">
                 <div className="temp">
                     <span className="error-message">수온 데이터를 불러올 수 없습니다.</span>
+                    <div className="temp__toStatistics">
+                        <a href="/" className="statistics__link">
+                            홈으로
+                        </a>
+                    </div>
                 </div>
                 <Background></Background>
                 <Footer></Footer>
