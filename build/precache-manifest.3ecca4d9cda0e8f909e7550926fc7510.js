08221ea166e048e68ea696b2778035750acd7ba6
Yang Taehwan <taehwan920@gmail.com>
2020-04-18 00:12:41 +0900

    Make React build

---
+++ build/precache-manifest.3ecca4d9cda0e8f909e7550926fc7510.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7eaed8308d0b9ceb37ce41a33e113fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da239bc0a60cfcf671c8",
+    "url": "/static/css/main.051f8813.chunk.css"
+  },
+  {
+    "revision": "8b1e03355b9961ee3cc9",
+    "url": "/static/js/2.e5066d6b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e5066d6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da239bc0a60cfcf671c8",
+    "url": "/static/js/main.397b9ce2.chunk.js"
+  },
+  {
+    "revision": "43223e5c2ae37ea7adf6",
+    "url": "/static/js/runtime-main.88fc6ba1.js"
+  },
+  {
+    "revision": "6b70dd7ec47b2bd1ecda6cb0159620d5",
+    "url": "/static/media/han-gang.6b70dd7e.jpg"
+  }
+]);
