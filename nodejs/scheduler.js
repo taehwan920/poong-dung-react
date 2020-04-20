241527388a4386096d61b5a61a59e09a21a1de60
Yang Taehwan <taehwan920@gmail.com>
2020-04-20 13:19:47 +0900

    emergency data insert

--- nodejs/scheduler.js
+++ nodejs/scheduler.js
@@ -17,6 +17,7 @@ const schedulerInit = () => {
             console.log(results)
         })
     }
+    schedule.scheduleJob('0 21 1 * * *', () => pyRun());
     schedule.scheduleJob('0 30 0 * * *', () => pyRun());
     schedule.scheduleJob('0 30 6 * * *', () => pyRun());
     schedule.scheduleJob('0 30 12 * * *', () => pyRun());
